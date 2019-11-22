19d49ab1d56a27e339c51fe46b885fef670d816e
Rosie <rosannainman@gmail.com>
2019-11-22 12:06:09 +0000

    First commit

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -9,3 +9,23 @@ describe("catalogueService", () => {
     });
   });
 });
+
+describe("catalogueService", () => {
+  describe("catalogueService.countBooksByAuthor", () => {
+    test("returns the total number of books written by the given author", () => {
+      expect(catalogueService.countBooksByAuthor("Hilary Mantel")).toBe(5);
+      expect(catalogueService.countBooksByAuthor("Celeste Ng")).toBe(2);
+      expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
+    });
+  });
+
+  describe("catalogueService.checkBookByTitle", () => {
+    test("returns true if the book exists", () => {
+      expect(
+        catalogueService.checkBookByTitle(
+          "The Assassination of Margaret Thatcher"
+        )
+      ).toBe(true);
+    });
+  });
+});
